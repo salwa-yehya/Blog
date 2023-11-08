0e9ec286e38ec691e5e4f1ba5d86933c75656d0b
salwa-yehya <salwayehyaabuafash@gmail.com>
2023-11-08 15:53:44 +0300

    starting

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import styles from "./homepage.module.css";
+
+export default function Home() {
+  return <div>
+    <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat pariatur eum neque voluptatibus, quasi aperiam nesciunt exercitationem distinctio modi eos? Accusamus architecto minus suscipit. Nam molestiae quidem accusamus? Eligendi, voluptas?</h1>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, cum asperiores! Sit dolorum quidem, aliquid dolorem neque totam perspiciatis, beatae voluptas provident qui iste sed cupiditate debitis nesciunt delectus. Ut!
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error harum vel possimus sapiente officia tempore est doloribus molestias veniam quod autem dolor culpa optio veritatis assumenda natus eligendi odio commodi earum, asperiores perspiciatis nisi repudiandae! At repellendus fugit illum eligendi, voluptatum sed eius aperiam id pariatur aspernatur, numquam modi deserunt facilis qui! Voluptatum veritatis est impedit veniam ex accusantium ab sed ipsum magnam minima consequuntur, repellat vero velit, aspernatur perspiciatis enim atque officiis similique odio doloremque eos? In sed debitis ex quisquam! Dolore nobis sit eos nam ad. Quo placeat est laboriosam tempore aliquid dicta voluptatibus pariatur, vitae modi! Unde?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium tempore modi dolor quo assumenda reprehenderit, ullam asperiores et ut sequi ea soluta molestiae ipsam quod? Dignissimos adipisci laboriosam labore enim temporibus tempore eveniet facere, veniam necessitatibus? Earum sit magnam eveniet tempore illo impedit reprehenderit, cumque eum, saepe corporis dolore officia adipisci id eligendi voluptatum fugiat consectetur rem ipsa suscipit animi architecto asperiores, perferendis repellendus. Amet facilis qui repellat eligendi. Accusamus dolorem voluptas laborum obcaecati eos voluptate. Ad odio hic itaque, eaque obcaecati a, aliquid reprehenderit vel adipisci ratione quos repellat porro! Cumque eligendi sint quod ut quaerat ab nemo eum.
+    <Link href='/'>link</Link>
+  
+  </div>;
+}
